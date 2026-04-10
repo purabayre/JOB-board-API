80d96d89646c1ecf023a22249ec7dee12b03348c
purabayre <purabayre293@gmail.com>
2026-04-10 16:56:50 +0530

    updated

--- src/cron/scheduler.js
+++ src/cron/scheduler.js
@@ -1,4 +1,6 @@
 const cron = require("node-cron");
+const mongoose = require("mongoose");
+
 const History = require("../models/history");
 
 cron.schedule("*/10 * * * * *", async () => {
@@ -7,6 +9,7 @@ cron.schedule("*/10 * * * * *", async () => {
     getUpdateDate.setDate(getUpdateDate.getDate() - 7);
 
     const result = await History.updateMany(
+      // update mnay first find which document to find , then Find all history records whose createdAt date is less than ($lt) getUpdateDate And isDeleted is false
       {
         createdAt: { $lt: getUpdateDate },
         isDeleted: false,
